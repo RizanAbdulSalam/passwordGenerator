c2a49a979f440df19f8df16ed21be60670d8c0ca
RizanAbdulSalam <82836256+RizanAbdulSalam@users.noreply.github.com>
2023-07-08 19:02:36 +0530

    Update index.js

--- Pass Gene/index.js
+++ Pass Gene/index.js
@@ -2,7 +2,6 @@ function getPassword(){
     var chars = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789qwertyuiopasdfghjklzxcvbnm!@#£€₹$%^&*()<>?{}\;[]|~`+=•←→"
     var passwordLength = 14;
     var password = "";
-
     for (var i=0; i<passwordLength; i++){
         var randomNumber = Math.floor(Math.random() *  chars.length);
         password += chars.substring(randomNumber,randomNumber+1);
